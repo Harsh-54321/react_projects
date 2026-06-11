8dd4216cef95cc976c093794d9359ab42cfc7846
Harsh-54321 <hr08051997@gmail.com>
2026-06-11 20:59:52 +0530

    this is update of date 11-06-2026

--- react-props/src/App.jsx
+++ react-props/src/App.jsx
@@ -6,12 +6,11 @@ const App = () => {
   return (
     <div className="container">
       <div className="flex-container">
-        
         {/* <Card user="rana harsh" /> */}
-        <Card name="rana harsh" />
-        <Card name="gupta vishal" />
-        <Card name="diptesh prajapti" />
-        <Card name="kamlesh kumavat" />
+        <Card name="rana harsh" age={18} img='https://images.unsplash.com/photo-1778409588703-e031779966b3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+        <Card name="gupta vishal" age={41} img='https://images.unsplash.com/photo-1702053174656-531b4d6c6960?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <Card name="diptesh prajapti" age={30} img='https://plus.unsplash.com/premium_photo-1670515870917-7e6d5065dba1?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <Card name="kamlesh kumavat" age={32} img='https://plus.unsplash.com/premium_photo-1768742409101-f2a31c9f761e?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
         {/* <Card name="rana harsh" /> */}
       </div>
     </div>
--- react-props/src/components/prop.jsx
+++ react-props/src/components/prop.jsx
@@ -6,9 +6,9 @@ const Prop = (users) => {
     return (
     <div className="card">
         <div className="img-wrap">
-        <img src="https://images.unsplash.com/photo-1778409588609-0d59abd6394d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <img src={users.img} alt="" />
         </div>
-        <h2>{users.name}</h2>
+        <h2>{users.name}-{users.age}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, rerum?</p>
         <a href="#" className='button btn-theme'>READ MORE</a>
     </div>
