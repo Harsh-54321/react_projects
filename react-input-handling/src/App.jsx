a5d5f0343d373ce0f361c71690a68bf93f228bfc
Harsh-54321 <hr08051997@gmail.com>
2026-06-30 23:14:53 +0530

    new Upload

---
+++ react-input-handling/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <section className='py-30 text-center bg-violet-500'>
+      <div className="container">
+         <h1>This is an H1</h1>
+      </div>
+    </section>  
+  )
+}
+
+export default App
