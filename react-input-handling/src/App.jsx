4eebad20344d273450a3143b82fc73aad09159b5
Harsh-54321 <hr08051997@gmail.com>
2026-07-01 22:42:45 +0530

    update

--- react-input-handling/src/App.jsx
+++ react-input-handling/src/App.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
+  const submitHandler = (e) => { 
+    e.preventDefault()
+    console.log("form Submited.");
+  }
   return (
     <section className='py-30 text-center bg-violet-500'>
       <div className="container">
-         <h1>This is an H1</h1>
+         <form action="" className='w-150 text-center mx-auto block' onSubmit={(e) => { 
+          submitHandler(e)
+         }} >
+            <input type="text"  placeholder='Enter Your Name' />
+            <button className='btn-theme button'>SUBMIT</button>
+         </form>
       </div>
     </section>  
   )
