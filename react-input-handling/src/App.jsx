7e46af98b1cb9994ff07daad3fd55e1b7e2cee92
Harsh-54321 <hr08051997@gmail.com>
2026-07-05 14:41:37 +0530

    this is an update

--- react-input-handling/src/App.jsx
+++ react-input-handling/src/App.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React,  { useState } from 'react'
 import './App.css'
 
 const App = () => {
+
+  const [title, setTitle] = useState('')
+
+  // This is an Function call on click
   const submitHandler = (e) => { 
     e.preventDefault()
     console.log("form Submited.");
+    e.target.value = "";
+    setTitle(e.target.value);
   }
+
+  // This is an Function call on input, on change
+  // const onChangeEevnt = (e) => {
+  //   e.preventDefault()
+    // console.log("Inputing");  //  shown colsole "inputing"
+    // console.log(e);  //  shown colsole "event"
+    // console.log(e.target);  //  shown colsole "targeted input"
+    // console.log(e.target.value)   //  shown colsole "inputing" value
+    // console.log("function is runing")
+    // setTitle(e.tarWget.value);
+  // }
+  
   return (
     <section className='py-30 text-center bg-violet-500'>
       <div className="container">
-         <form action="" className='w-150 text-center mx-auto block' onSubmit={(e) => { 
-          submitHandler(e)
-         }} >
-            <input type="text"  placeholder='Enter Your Name' />
-            <button className='btn-theme button'>SUBMIT</button>
-         </form>
+        <form action="" className='w-150 text-center mx-auto block' 
+          onSubmit={(e) => {
+            console.log("runing. ..."); 
+            submitHandler(e)
+          }} 
+        >
+
+          {/* this is an expmle of Tow way binding  */}
+          
+          <input type="text"  placeholder='Enter Your Name' 
+            onChange={(e) => {
+              console.log("runing. ...");
+              setTitle(e.target.value)
+            }} 
+            value={title} 
+          />
+          <button className='btn-theme button'>
+            SUBMIT
+          </button>
+        </form>
       </div>
     </section>  
   )
